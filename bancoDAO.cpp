1ba38ab9435f80c3e7d3460584ee73f55ec61dac
Alinne Alessandra <69517722+alinnealess@users.noreply.github.com>
2023-11-26 01:26:50 -0300

    26-11b

--- bancoDAO.cpp
+++ bancoDAO.cpp
@@ -59,7 +59,7 @@ void BancoDAO::listarProfessores() const
 {
     for (const Professor &professor : professoresvector)
     {
-        
+
         cout << professor.getFormacao() << " " << professor.getNivel() << " "
              << professor.getDisciplina() << " " << professor.getMatricula()
              << " " << professor.getSalario() << " "
@@ -110,21 +110,23 @@ void BancoDAO::deletarProfessor(string matricula)
 // Função para salvar os dados dos professores no arquivo
 void BancoDAO::salvarArquivoProfessores()
 {
-     ofstream arquivo("professores.txt");
-  if (arquivo.is_open()) {
-    for (const Professor &professor : professoresvector) {
-      arquivo << professor.getFormacao() << " " << professor.getNivel() << " "
-             << professor.getDisciplina() << " " << professor.getMatricula()
-             << " " << professor.getSalario() << " "
-             << professor.getDepartamento() << " " << professor.getCargaHoraria()
-             << " " << professor.getDataIngresso() << " " << professor.getNome()
-             << " " << professor.getCpf() << " " << professor.getDataNascimento()
-             << " " << professor.getGenero() << " " << professor.getRua() << " "
-             << professor.getNumero() << " " << professor.getBairro() << " "
-             << professor.getCidade() << " " << professor.getCep() << endl;
+    ofstream arquivo("professores.txt");
+    if (arquivo.is_open())
+    {
+        for (const Professor &professor : professoresvector)
+        {
+            arquivo << professor.getFormacao() << " " << professor.getNivel() << " "
+                    << professor.getDisciplina() << " " << professor.getMatricula()
+                    << " " << professor.getSalario() << " "
+                    << professor.getDepartamento() << " " << professor.getCargaHoraria()
+                    << " " << professor.getDataIngresso() << " " << professor.getNome()
+                    << " " << professor.getCpf() << " " << professor.getDataNascimento()
+                    << " " << professor.getGenero() << " " << professor.getRua() << " "
+                    << professor.getNumero() << " " << professor.getBairro() << " "
+                    << professor.getCidade() << " " << professor.getCep() << endl;
+        }
+        arquivo.close();
     }
-    arquivo.close();
-  }
 }
 
 // ===Funções relacionadas a Técnicos Administrativos (TecnicoADM)===
@@ -132,29 +134,126 @@ void BancoDAO::salvarArquivoProfessores()
 // Função para ler os dados dos Técnicos ADM do arquivo
 void BancoDAO::lerArquivoTecnicoAdm()
 {
-}
+    ifstream arquivo("tecnicosADM.txt");
+    if (arquivo.is_open())
+    {
+        float adicionalProdutividade;
+        string funcaoDesempenhada;
+        string matricula;
+        float salario;
+        string departamento;
+        int cargaHoraria;
+        string dataIngresso;
+        string nome;
+        string cpf;
+        string dataNascimento;
+        string genero;
+        string rua;
+        int numero;
+        string bairro;
+        string cidade;
+        string cep;
 
+        while (arquivo >> adicionalProdutividade >> funcaoDesempenhada >> matricula >> salario >>
+               departamento >> cargaHoraria >> dataIngresso >> nome >> cpf >>
+               dataNascimento >> genero >> rua >> numero >> bairro >> cidade >>
+               cep)
+        {
+            TecnicoAdm tecnicoADM(adicionalProdutividade, funcaoDesempenhada, matricula, salario,
+                                  departamento, cargaHoraria, dataIngresso, nome,
+                                  cpf, dataNascimento, genero, rua, numero, bairro,
+                                  cidade, cep);
+            tecnicosADMvector.push_back(tecnicoADM);
+        }
+        arquivo.close();
+    }
+}
 // Função para salvar os dados dos Técnicos ADM no arquivo
 void BancoDAO::salvarArquivoTecnicoAdm()
 {
+    ofstream arquivo("tecnicosADM.txt");
+    if (arquivo.is_open())
+    {
+        for (const TecnicoAdm &tecnicoADM : tecnicosADMvector)
+        {
+            arquivo << tecnicoADM.getAdicionalProdutividade() << " " << tecnicoADM.getFuncaoDesempenhada() << " "
+                    << tecnicoADM.getMatricula() << " " << tecnicoADM.getSalario()
+                    << " " << tecnicoADM.getDepartamento() << " "
+                    << tecnicoADM.getCargaHoraria() << " "
+                    << tecnicoADM.getDataIngresso() << " " << tecnicoADM.getNome()
+                    << " " << tecnicoADM.getCpf() << " "
+                    << tecnicoADM.getDataNascimento() << " " << tecnicoADM.getGenero()
+                    << " " << tecnicoADM.getRua() << " " << tecnicoADM.getNumero()
+                    << " " << tecnicoADM.getBairro() << " " << tecnicoADM.getCidade()
+                    << " " << tecnicoADM.getCep() << endl;
+        }
+        arquivo.close();
+    }
 }
 
 // Função para cadastrar um novo Técnico ADM
-void BancoDAO::cadastrarTecnicoADM(const TecnicoAdm tecnicoADM)
+void BancoDAO::cadastrarTecnicoADM(TecnicoAdm novoTecnicoADM)
 {
+    tecnicosADMvector.push_back(novoTecnicoADM);
+    salvarArquivoTecnicoAdm();
 }
 
 // Função para listar todos os Técnicos ADM
 void BancoDAO::listarTecnicosADM() const
 {
+    for (const TecnicoAdm &tecnicoADM : tecnicosADMvector)
+    {
+        // Print or manipulate technical administrative staff data here
+        cout << tecnicoADM.getAdicionalProdutividade() << " "
+             << tecnicoADM.getFuncaoDesempenhada() << " "
+             << tecnicoADM.getMatricula() << " "
+             << tecnicoADM.getSalario() << " "
+             << tecnicoADM.getDepartamento() << " "
+             << tecnicoADM.getCargaHoraria() << " "
+             << tecnicoADM.getDataIngresso() << " " << tecnicoADM.getNome()
+             << " " << tecnicoADM.getCpf() << " "
+             << tecnicoADM.getDataNascimento() << " " << tecnicoADM.getGenero()
+             << " " << tecnicoADM.getRua() << " " << tecnicoADM.getNumero()
+             << " " << tecnicoADM.getBairro() << " " << tecnicoADM.getCidade()
+             << " " << tecnicoADM.getCep() << endl;
+    }
 }
 
 // Função para deletar um Técnico ADM pelo número de matrícula
 void BancoDAO::deletarTecnicoADM(string matricula)
 {
+    for (auto it = tecnicosADMvector.begin(); it != tecnicosADMvector.end(); ++it)
+    {
+        if (it->getMatricula() == matricula)
+        {
+            tecnicosADMvector.erase(it);
+            break;
+        }
+    }
 }
 
 // Função para buscar um Técnico ADM pelo número de matrícula
 void BancoDAO::buscarTecnicoADM(string matricula) const
 {
+    for (const TecnicoAdm &tecnicoADM : tecnicosADMvector)
+    {
+        if (tecnicoADM.getMatricula() == matricula)
+        {
+            // Found the technical administrative staff, you can perform actions here
+            // For example, output the details of the matched staff
+            cout << tecnicoADM.getAdicionalProdutividade() << " "
+                 << tecnicoADM.getFuncaoDesempenhada() << " "
+                 << tecnicoADM.getMatricula() << " "
+                 << tecnicoADM.getSalario() << " "
+                 << tecnicoADM.getDepartamento() << " "
+                 << tecnicoADM.getCargaHoraria() << " "
+                 << tecnicoADM.getDataIngresso() << " " << tecnicoADM.getNome()
+                 << " " << tecnicoADM.getCpf() << " "
+                 << tecnicoADM.getDataNascimento() << " " << tecnicoADM.getGenero()
+                 << " " << tecnicoADM.getRua() << " " << tecnicoADM.getNumero()
+                 << " " << tecnicoADM.getBairro() << " " << tecnicoADM.getCidade()
+                 << " " << tecnicoADM.getCep() << endl;
+            return;
+        }
+    }
 }
